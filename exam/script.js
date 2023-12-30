81a84b571c4e1ebce40836ccc0ab389cfe5b2b0d
AlinaBatyuk <93945034+AlinaBatyuk@users.noreply.github.com>
2023-12-30 11:48:19 +0200

    Add files via upload

--- exam/script.js
+++ exam/script.js
@@ -1,17 +1,15 @@
 
 const fahrenheitInput = document.getElementById('fahrenheitInput');
 
-        celsiusInput.addEventListener("click", function () {
-            const fahrenheit = parseFloat(fahrenheitInput.value);
-            console.log("yes")
-            if (!isNaN(fahrenheit) & 1<=fahrenheit & fahrenheit<=100) {
-                const resultElement1 = document.getElementById("result");
-                const input = document.getElementById("fahrenheitInput");
-                input.classList = "correct_input";
-            } else {
-                const resultElement = document.getElementById("result");
-                const input = document.getElementById("fahrenheitInput");
-                input.classList = "false_input";
-            }
-        
-    });
+        function validateNumber() {
+            const inputElement = document.getElementById('fahrenheitInput');
+            const fahrenheit = parseInt(fahrenheitInput.value);
+
+             if (!isNaN(fahrenheit) && fahrenheit >= 1 && fahrenheit <= 100) {
+                    inputElement.classList.remove('false_input');
+                    inputElement.classList.add('correct_input');
+                } else {
+                    inputElement.classList.remove('correct_input');
+                    inputElement.classList.add('false_input');
+                }
+    };
